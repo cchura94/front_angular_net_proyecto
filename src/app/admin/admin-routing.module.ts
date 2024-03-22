42c6f7c691015524e663e5ef237ae7c8f9812c56
cristian <cchura.cpc@gmail.com>
2024-03-21 22:33:34 -0400

    componente navbar

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,18 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from '../core/guards/auth.guard';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'perfil',
-    component: PerfilComponent,
-    canActivate: [authGuard]
-  },
+    component: NavbarComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+        canActivate: [authGuard]
+      },
+      {
+        path: 'perfil',
+        component: PerfilComponent,
+        canActivate: [authGuard]
+      },
+    ]
+  }
 ];
 
 @NgModule({
